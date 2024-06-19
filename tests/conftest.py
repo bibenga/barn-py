e41539a953df436cc0a23e27a2fd3cae001fd64b
bibenga <{ID}+{username}@users.noreply.github.com>
2024-06-19 21:47:42 +0000

    1

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import os
+from typing import Generator
+
+import pytest
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from barn.models import metadata
+
+
+@pytest.fixture(scope="session")
+def engine() -> Generator[Engine, None, None]:
+    try:
+        os.remove("db_test.sqlite3")
+    except FileNotFoundError:
+        pass
+    engine = create_engine('sqlite:///db_test.sqlite3')
+    # engine = create_engine('postgresql+psycopg://rds:sqlsql@host.docker.internal/barn')
+    # with engine.begin():
+    #     # Base.metadata.drop_all(engine)
+    #     # Base.metadata.create_all(engine)
+    #     metadata.drop_all(engine)
+    #     metadata.create_all(engine)
+    yield engine
+    os.remove("db_test.sqlite3")
+
+
+@pytest.fixture
+def session_ctx(engine) -> Generator[sessionmaker[Session], None, None]:
+    session_ctx = sessionmaker(engine)
+    yield session_ctx
+
+
+@pytest.fixture(scope="function")
+def session(engine: Engine, session_ctx: sessionmaker[Session]) -> Generator[Session, None, None]:
+    metadata.create_all(engine)
+    try:
+        with session_ctx() as session:
+            yield session
+    finally:
+        metadata.drop_all(engine)
+
